163209a6f83ea50d21c008d934b98c0fd00b8e0f
Massimo <evasi76@hotmail.com>
2021-09-14 18:36:42 +0000

    add onload in body

--- script.js
+++ script.js
@@ -5,20 +5,28 @@
 const MINVAL = 1;
 const MAXVAL = 20;
 const DEFAULT_VAL = 3.5;
-const STEP=0.25
-// get element DOM
-var amount = document.getElementById("principal");
-var rate = document.getElementById("rate");
-var years = document.getElementById("years");
-var rateval = document.getElementById("rate_val");
-// init form values rate field
-if(rate){
-    //#myElementID element DOES exists
+const STEP = 0.25;
+
+// variables
+var amount;
+var rate;
+var years;
+var rateval;
+
+// inizialization element
+function init(){
+  // get element DOM
+  amount = document.getElementById("principal");
+  rate = document.getElementById("rate");
+  years = document.getElementById("years");
+  rateval = document.getElementById("rate_val");
+  //#myElementID element DOES exists
   rate.setAttribute("max", MAXVAL);
   rate.setAttribute("min", MINVAL);
   rate.setAttribute("value", DEFAULT_VAL);
   rate.setAttribute("step", STEP);
   rateval.value = DEFAULT_VAL + '%';
+  
 }
 
 function compute()
@@ -30,13 +38,13 @@ function compute()
         return false;
     }
     // set value
-    var principalVal = amount.value;
-    var rateVal = rate.value;
-    var yearsVal = years.value;
-    var year = new Date().getFullYear() + parseInt(years.value);
-    var interest = principalVal * yearsVal * rateVal /100 ;
+	var principalVal = amount.value;
+  var rateVal = rate.value;
+  var yearsVal = years.value;
+  var year = new Date().getFullYear() + parseInt(years.value);
+	var interest = principalVal * yearsVal * rateVal /100 ;
     // inner in element html response function
-    document.getElementById("result").innerHTML="If you deposit <mark>"+principalVal+",\</mark><br\>at an interest rate of <mark>"+rateVal+"%\</mark><br\>You will receive an amount of <mark>"+interest+",\</mark><br\>in the year <mark>"+year+"\</mark><br\>"
+	document.getElementById("result").innerHTML="If you deposit <mark>"+principalVal+",\</mark><br\>at an interest rate of <mark>"+rateVal+"%\</mark><br\>You will receive an amount of <mark>"+interest+",\</mark><br\>in the year <mark>"+year+"\</mark><br\>"
 }
 
 // add reset form
