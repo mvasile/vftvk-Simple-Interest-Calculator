7a533846fdac384ef68d69bc1f2c54083835fffd
Massimo <evasi76@hotmail.com>
2021-09-14 17:56:03 +0000

    add check

--- script.js
+++ script.js
@@ -11,15 +11,15 @@ var amount = document.getElementById("principal");
 var rate = document.getElementById("rate");
 var years = document.getElementById("years");
 var rateval = document.getElementById("rate_val");
-
-setTimeout(function(){
-  // init form values rate field
+// init form values rate field
+if(rate){
+    //#myElementID element DOES exists
   rate.setAttribute("max", MAXVAL);
   rate.setAttribute("min", MINVAL);
   rate.setAttribute("value", DEFAULT_VAL);
   rate.setAttribute("step", STEP);
   rateval.value = DEFAULT_VAL + '%';
-},500);
+}
 
 function compute()
 {
@@ -30,13 +30,13 @@ function compute()
         return false;
     }
     // set value
-	var principalVal = amount.value;
+    var principalVal = amount.value;
     var rateVal = rate.value;
     var yearsVal = years.value;
     var year = new Date().getFullYear() + parseInt(years.value);
-	var interest = principalVal * yearsVal * rateVal /100 ;
+    var interest = principalVal * yearsVal * rateVal /100 ;
     // inner in element html response function
-	document.getElementById("result").innerHTML="If you deposit <mark>"+principalVal+",\</mark><br\>at an interest rate of <mark>"+rateVal+"%\</mark><br\>You will receive an amount of <mark>"+interest+",\</mark><br\>in the year <mark>"+year+"\</mark><br\>"
+    document.getElementById("result").innerHTML="If you deposit <mark>"+principalVal+",\</mark><br\>at an interest rate of <mark>"+rateVal+"%\</mark><br\>You will receive an amount of <mark>"+interest+",\</mark><br\>in the year <mark>"+year+"\</mark><br\>"
 }
 
 // add reset form
